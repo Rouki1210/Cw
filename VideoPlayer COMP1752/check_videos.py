c11943e8d55bf04155fd1f3faf1baa32d5937704
Vinh Nguyen <vinh12102004@gmail.com>
2023-11-30 18:32:53 +0700

    new update clickable name

--- VideoPlayer COMP1752/check_videos.py
+++ VideoPlayer COMP1752/check_videos.py
@@ -27,6 +27,7 @@ class CheckVideos():
 
         self.list_txt = tkst.ScrolledText(window, width=48, height=12, wrap="none")     #Create a table list all videos
         self.list_txt.grid(row=1, column=0, columnspan=3, sticky="W", padx=10, pady=10) 
+        self.list_txt.tag_configure('clickable')
 
         self.video_txt = tk.Text(window, width=24, height=4, wrap="none")   #Create a text of info video in list video 
         self.video_txt.grid(row=1, column=3, sticky="NW", padx=10, pady=10)
@@ -49,11 +50,28 @@ class CheckVideos():
         else:
             set_text(self.video_txt, f"Video {key} not found")  # Text when not found video
         self.status_lbl.configure(text="Check Video button was clicked!") 
-        
-    #Comment code    
+    
+    def click_name_in_list(self, event):
+        index = self.list_txt.index(tk.CURRENT)
+        key = index.split(".")[0].zfill(2)
+        name = lib.get_name(key)  
+        if name is not None:        # when find this video
+            director = lib.get_director(key)        # get dir this video
+            rating = lib.get_rating(key)            # get rate this video
+            play_count = lib.get_play_count(key)    # get play count this video
+            video_details = f"{name}\n{director}\nrating: {rating}\nplays: {play_count}"    # Detail video
+            set_text(self.video_txt, video_details) 
+        else:
+            set_text(self.video_txt, f"Video {key} not found")  # Text when not found video
+        self.status_lbl.configure(text="Check Video button was clicked!") 
+      
+    
+     
     def list_videos_clicked(self):
         video_list = lib.list_all()                     # Get list of all videos from library
         set_text(self.list_txt, video_list)             # Update the list widget with video list
+        self.list_txt.tag_add("clickable", "1.0", "end")
+        self.list_txt.tag_bind("clickable", "<Button-1>", self.click_name_in_list)
         self.status_lbl.configure(text="List Videos button was clicked!")   # Display status message
 
 if __name__ == "__main__":  # only runs when this file is run as a standalone
--- VideoPlayer COMP1752/create_video_demo.py
+++ VideoPlayer COMP1752/create_video_demo.py
@@ -1,69 +1,82 @@
 import tkinter as tk
+import tkinter.scrolledtext as tkst
+import video_library as lib
+import font_manager as fonts
 
-class UpdateVideosApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Update Videos App")
 
-        self.video_dict = {
-            1: {"name": "Video 1", "rating": 3.5, "play_count": 10},
-            2: {"name": "Video 2", "rating": 4.2, "play_count": 8},
-            3: {"name": "Video 3", "rating": 4.8, "play_count": 12},
-            # Add more videos as needed
-        }
+def set_text(text_area, content):
+    text_area.delete("1.0", tk.END)
+    text_area.insert(1.0, content)
 
-        self.create_widgets()
 
-    def create_widgets(self):
-        # Video number entry
-        self.video_number_label = tk.Label(self.root, text="Enter Video Number:")
-        self.video_number_label.pack()
+class CheckVideos():
+    def __init__(self, window):
+        window.geometry("750x350")
+        window.title("Check Videos")
 
-        self.video_number_entry = tk.Entry(self.root)
-        self.video_number_entry.pack()
+        list_videos_btn = tk.Button(window, text="List All Videos", command=self.list_videos_clicked)
+        list_videos_btn.grid(row=0, column=0, padx=10, pady=10)
 
-        # New rating entry
-        self.rating_label = tk.Label(self.root, text="Enter New Rating:")
-        self.rating_label.pack()
+        self.input_txt = tk.Entry(window, width=3)
+        self.input_txt.grid(row=0, column=2, padx=10, pady=10)
 
-        self.rating_entry = tk.Entry(self.root)
-        self.rating_entry.pack()
+        check_video_btn = tk.Button(window, text="Check Video", command=self.check_video_clicked)
+        check_video_btn.grid(row=0, column=3, padx=10, pady=10)
 
-        # Update video button
-        self.update_button = tk.Button(self.root, text="Update Video", command=self.update_video)
-        self.update_button.pack()
+        self.list_txt = tkst.ScrolledText(window, width=48, height=12, wrap="none")
+        self.list_txt.grid(row=1, column=0, columnspan=3, sticky="W", padx=10, pady=10)
 
-        # Display message label
-        self.message_label = tk.Label(self.root, text="")
-        self.message_label.pack()
+        # Configure the "hover" tag
 
-    def update_video(self):
-        try:
-            video_number = int(self.video_number_entry.get())
-            new_rating = float(self.rating_entry.get())
+        self.video_txt = tk.Text(window, width=24, height=4, wrap="none")
+        self.video_txt.grid(row=1, column=3, sticky="NW", padx=10, pady=10)
 
-            if video_number in self.video_dict:
-                video_info = self.video_dict[video_number]
-                video_name = video_info["name"]
-                play_count = video_info["play_count"]
+        self.status_lbl = tk.Label(window, text="", font=("Helvetica", 10))
+        self.status_lbl.grid(row=2, column=0, columnspan=4, sticky="W", padx=10, pady=10)
 
-                # Update video information
-                video_info["rating"] = new_rating
+        self.list_videos_clicked()
 
-                # Display message
-                message = f"Video Name: {video_name}\nNew Rating: {new_rating}\nPlay Count: {play_count}"
-                self.message_label.config(text=message, fg="green")
+    def click_name_in_list(self, event):
+        index = self.list_txt.index(tk.CURRENT)
+        key = index.split(".")[0].zfill(2)  # Ensure the key has leading zeros
+        self.display_video_info(key)
 
-            else:
-                self.display_error_message("Invalid video number.")
+    def check_video_clicked(self):
+        key = self.input_txt.get().zfill(2)  # Ensure the key has leading zeros
+        self.display_video_info(key)
 
-        except ValueError:
-            self.display_error_message("Invalid input. Please enter a valid number for rating.")
+    def display_video_info(self, key):
+        name = lib.get_name(key)
+        if name is not None:
+            director = lib.get_director(key)
+            rating = lib.get_rating(key)
+            play_count = lib.get_play_count(key)
+            video_details = f"{name}\n{director}\nrating: {rating}\nplays: {play_count}"
+            set_text(self.video_txt, video_details)
+        else:
+            set_text(self.video_txt, f"Video {key} not found")
+        self.status_lbl.configure(text="Video information displayed!")
 
-    def display_error_message(self, message):
-        self.message_label.config(text=message, fg="red")
+    def list_videos_clicked(self):
+        video_list = lib.list_all()
+        set_text(self.list_txt, video_list)
+
+        # Bind events to the "hover" tag
+        self.list_txt.tag_add("hover", "1.0", "end")
+        self.list_txt.tag_bind("hover", "<Enter>", self.on_enter)
+        self.list_txt.tag_bind("hover", "<Leave>", self.on_leave)
+
+        self.status_lbl.configure(text="List Videos button was clicked!")
+
+    def on_enter(self, event):
+        self.list_txt.tag_configure("hover", background="blue", foreground="white")
+
+    def on_leave(self, event):
+        self.list_txt.tag_configure("hover", background="white", foreground="black")
+
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = UpdateVideosApp(root)
-    root.mainloop()
+    window = tk.Tk()
+    fonts.configure()
+    CheckVideos(window)
+    window.mainloop()
